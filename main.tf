f4b2211b7838464a1bb8c7fd27cea7825d1bb6f2
Matt Scafe <scafeman@gmail.com>
2025-08-06 19:59:12 -0500

    - Fix vmname formatting error by correctly passing arguments to format() function - Updated all instances of format("${var.vmname}${var.vmnameformat}", ...) to format(var.vmnameformat, var.vmname, ...), ensuring proper handling of multi-placeholder format strings like "%s-%02d".

--- main.tf
+++ main.tf
@@ -88,7 +88,7 @@ locals {
 resource "vsphere_virtual_machine" "vm" {
   count      = var.instances
   depends_on = [var.vm_depends_on]
-  name       = "${var.staticvmname != null ? var.staticvmname : format("${var.vmname}${var.vmnameformat}", count.index + var.vmstartcount)}${var.fqdnvmname == true ? ".${var.domain}" : ""}"
+  name       = "${var.staticvmname != null ? var.staticvmname : format(var.vmnameformat, var.vmname, count.index + var.vmstartcount)}${var.fqdnvmname == true ? ".${var.domain}" : ""}"
 
   resource_pool_id        = var.vmrp != "" ? data.vsphere_resource_pool.pool[0].id : var.vmrpid
   folder                  = var.vmfolder
@@ -243,7 +243,7 @@ resource "vsphere_virtual_machine" "vm" {
         dynamic "linux_options" {
           for_each = var.is_windows_image ? [] : [1]
           content {
-            host_name     = var.staticvmname != null ? var.staticvmname : format("${var.vmname}${var.vmnameformat}", count.index + var.vmstartcount)
+            host_name     = var.staticvmname != null ? var.staticvmname : format(var.vmnameformat, var.vmname, count.index + var.vmstartcount)
             domain        = var.domain
             hw_clock_utc  = var.hw_clock_utc
             script_text   = var.script_text
@@ -253,7 +253,7 @@ resource "vsphere_virtual_machine" "vm" {
         dynamic "windows_options" {
           for_each = var.is_windows_image ? [1] : []
           content {
-            computer_name         = var.staticvmname != null ? var.staticvmname : format("${var.vmname}${var.vmnameformat}", count.index + var.vmstartcount)
+            computer_name         = var.staticvmname != null ? var.staticvmname : format(var.vmnameformat, var.vmname, count.index + var.vmstartcount)
             admin_password        = var.local_adminpass
             workgroup             = var.workgroup
             join_domain           = var.windomain
